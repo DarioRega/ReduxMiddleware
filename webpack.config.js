c3c10f7c8a461c10d8a0552434ef0fd807ff54cb
Dario <dario.regazzoni@outlook.fr>
2019-11-19 22:31:52 +0100

    init app and setup webpack and nodes modules necessary

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    // permet de dire a webpack que l'on fait du client side rendering 
+    // et qu'il ne faut pas qu' il essai d'appeller le serveur
+    // du coup on rend index.html à chaque fois
+    // dev only purpose
+    historyApiFallback: true
+  }
+};
